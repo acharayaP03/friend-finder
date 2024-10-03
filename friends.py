26d115556501a2694f6a72ded1a8b758378e261f
acharayaP03 <acharyaP03@gmail.com>
2024-10-03 17:57:25 +1000

    find matching friends

--- friends.py
+++ friends.py
@@ -18,13 +18,14 @@ def read_names_from_file(filename):
     with open(filename, 'r') as filename:
         return [line.strip() for line in filename]
 
-# with open('nearby_friends.txt', 'r') as file:
-#     contents = file.readlines()
-#     formatted_contents = [name.strip() for name in contents]
-#
-# print(formatted_contents)
-
-
+def find_all_matching_friends(friends, friends_name):
+    matches = []
+    for friend in friends_name:
+        for search in friends:
+            if search.lower() in friend.lower():
+                matches.append(friend)
+                break
+    return matches
 
 def main():
     file_name = 'nearby_friends.txt'
